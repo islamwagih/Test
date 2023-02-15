491c23e642254bec92f4d273097e7aee41cd99ed
Islam wagih <60822658+islamwagih@users.noreply.github.com>
2023-02-15 20:51:09 +0200

    test2

--- main.cpp
+++ main.cpp
@@ -9,9 +9,20 @@ int getSum(vector<int> nums)
   return sum;
 }
 
+int getMin(vector<int> nums)
+{
+    int minIdx = 0;
+    for(int i=1;i<nums.size();i++)
+    {
+        if(nums[i] < nums[minIdx]) minIdx = i;
+    }
+    return nums[minIdx];
+}
+
 int main()
 {
   vector<int> vec{4,5,1,2,3,-1,5,0,2,1};
   cout<<getSum(vec)<<endl;
+  cout<<getMin(vec)<<endl;
   return 0;
 }
